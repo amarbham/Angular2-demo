dba0f199319b21966e895a5ffbd5a6b929ef9e85
Amar Bhamra <amarbham@hotmail.com>
2016-11-18 16:41:14 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import 'purecss/build/pure-min.css'
+import 'purecss/build/grids-min.css'
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+
+console.info('app.environment:', app.environment);
+if (app.environment === 'production') {
+  enableProdMode();
+}
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path'),
+    webpack = require('webpack'),
+    htmlWebpackPlugin = require('html-webpack-plugin'),
+    CopyWebpackPlugin = require('copy-webpack-plugin'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: "./src/main.ts",
+        polyfills: [
+            'core-js/es6',
+            'core-js/es7/reflect',
+            'zone.js/dist/zone'
+        ]
+    },
+    output: {
+        path: './dist',
+        filename: '[name].[hash].js',
+    },
+    module: {
+        loaders: [{
+            test: /\.component.ts$/,
+            loader: 'ts-loader!angular2-template'
+        }, {
+            test: /\.tsx?$/,
+            exclude: /\.component.ts$/,
+            loader: 'ts-loader'
+        }, {
+            test: /\.html$/,
+            loader: 'raw-loader'
+        }, {
+            test: /\.css$/,
+            loader: 'raw-loader'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style', 'css-loader')
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts', '.html', '.css']
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new webpack.DefinePlugin({
+            app: {
+                environment: JSON.stringify(process.env.APP_ENVIRONMENT || 'development')
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'polyfills'
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/images',
+            to: 'images'
+        }]),
+        new ExtractTextPlugin('[name].css')
+    ]
+};
