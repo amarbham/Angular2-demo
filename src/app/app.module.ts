37dde5d5cdcf33758512863bb366bcf9cf2a2916
Amar Bhamra <amarbham@hotmail.com>
2016-11-22 16:31:43 +0000

    include forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
 import {ArtistListComponent} from './artist/artist-list.component'
 import {ArtistDetailComponent} from './artist/artist-detail.component'
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, ArtistListComponent, ArtistDetailComponent],
   bootstrap: [AppComponent]
 })
