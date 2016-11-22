b89c6406831f3463d0d6f736f11628e454d9be63
Amar Bhamra <amarbham@hotmail.com>
2016-11-22 16:45:43 +0000

    add click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,18 @@ import { ArtistDetailComponent } from './artist/artist-detail.component';
   providers: [ArtistService],
   template: `
     <div class="container">
-      <artist-list></artist-list>
-      <artist-detail></artist-detail>
+      <artist-list *ngIf="state ==='artist-list'" 
+        (artistSelect)="showArtistDetail($event)">
+       </artist-list>
+      <artist-detail *ngIf="state === 'artist-detail'"></artist-detail>
     </div>
     `
 })
 
 export class AppComponent {
-
+  state = 'artist-list';
+  
+  showArtistDetail(artistId){
+    this.state = 'artist-detail';
+  }
 }
