e7f60cb4e4b88b27b379da22a8b5b914f078de21
Amar Bhamra <amarbham@hotmail.com>
2016-11-23 09:41:17 +0000

    remove artist component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
-import { ArtistService } from './artist/artist.service';
-import { ArtistListComponent } from './artist/artist-list.component';
-import { ArtistDetailComponent } from './artist/artist-detail.component';
-
 
 @Component({
-  selector: 'angular-tunes',
-  providers: [ArtistService],
+  selector: '',
+  providers: [],
   template: `
-    <div class="container">
-      <artist-list *ngIf="state ==='artist-list'" 
-        (artistSelect)="showArtistDetail($event)">
-       </artist-list>
-      <artist-detail *ngIf="state === 'artist-detail'"></artist-detail>
-    </div>
+
     `
 })
 
 export class AppComponent {
-  state = 'artist-list';
-  
-  showArtistDetail(artistId){
-    this.state = 'artist-detail';
-  }
+
 }
