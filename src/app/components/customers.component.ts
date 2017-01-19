bdf8d0bd4c859fc41f872d93026137baa955fc29
Amar Bhamra <amarbham@hotmail.com>
2017-01-19 10:47:03 +0000

    added a customers component

---
+++ src/app/components/customers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'customers',
+  providers: [],
+  template: require('./customers.template.html')
+})
+
+export class CustomersComponent {
+    title = 'this is a the customers Componnt'
+}
