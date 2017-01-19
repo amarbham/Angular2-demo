fa2dea9cb6b2e97c17b06b989ed597965dad1d61
Amar Bhamra <amarbham@hotmail.com>
2017-01-19 10:56:54 +0000

    change customers to users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CustomersComponent } from './components/customers.component';
+import { UsersComponent } from './components/users.component';
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, CustomersComponent],
+  declarations: [AppComponent, UsersComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'users',
+  providers: [],
+  template: require('./users.template.html')
+})
+
+export class UsersComponent {
+    title = 'this is a the users Componnt'
+
+}
