782af760f808b2cce3d4494cb5909488e1cca882
Amar Bhamra <amarbham@hotmail.com>
2017-01-24 15:06:34 +0000

    refactor counter component

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -18,31 +18,29 @@ export class CounterComponent implements OnInit {
     
   next(counterValue: number) {
 
-    if (counterValue >= this.counterData.length - 1) {
-      this.currentSelected = this.counterData[0]
-      return this.counterValue = 0;
-    }
-    this.counterValue++
-    this.currentSelected = this.counterData[this.counterValue];
+    if (counterValue >= this.counterData.length - 1) return 
 
+    this.counterValue++
+   
     this.counterChange.emit({
       value: this.counterValue,
-      selected: this.currentSelected
+      selected: this.currentSelected = this.counterData[this.counterValue]
     })
   }
 
   previous(counterValue: number) {
+    if (counterValue <= 0 ) return;
 
-    if(counterValue <= 0){
-      this.currentSelected = this.counterData[this.counterData.length - 1]
-      return this.counterValue = this.counterData.length - 1
-    }
     this.counterValue--
-    this.currentSelected = this.counterData[this.counterValue]
 
+    // if(counterValue <= 0){
+    //   this.currentSelected = this.counterData[this.counterData.length - 1]
+    //   return this.counterValue = this.counterData.length - 1
+    // }
+    
     this.counterChange.emit({
       value: this.counterValue,
-      selected: this.currentSelected
+      selected: this.currentSelected = this.counterData[this.counterValue]
     })
   }
 
