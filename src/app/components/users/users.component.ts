afbb94f5b42906d5a03eec053efb42e64e38d32e
Amar Bhamra <amarbham@hotmail.com>
2017-01-31 14:01:21 +0000

    refactor updated user

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -55,9 +55,12 @@ export class UsersComponent implements OnInit {
       .toPromise()
       .then(() => this.getUsers())
       .then(() => {
+
         for (let x in this.selectedUser) {
-          this.selectedUser[x] = null
+          if (this.selectedUser.hasOwnProperty(x)) 
+            this.selectedUser[x] = null
         }
+
         this.form.reset()
 
         if (this.users.length > 1) {
@@ -67,26 +70,16 @@ export class UsersComponent implements OnInit {
       .catch(error => this.errorMessage = `${error}: Could not delete user. Please try again.`);
   }
 
-  updateUser(user: User) {
-    const updatedUser: User = {
-      id: this.selectedUser.id,
-      name: this.form.value.name,
-      email: this.form.value.email,
-      telephone_number: this.form.value.telephone_number
-    }
+  updateUser(form: FormGroup) {
+
+    const updatedUser =
+      Object.assign({ id: this.selectedUser.id }, form.value)
 
     this.dataService.update(this.usersUrl + updatedUser.id, updatedUser)
       .toPromise()
-      .then(() => {
-        return this.users = this.users.map((user: User) => {
-          if (user.id == updatedUser.id) {
-            user.name = updatedUser.name;
-            user.email = updatedUser.email;
-            user.telephone_number = updatedUser.telephone_number;
-          }
-          return user;
-        })
-      })
+      .then(() => this.getUsers())
+      .then(() => this.selectedUser = updatedUser)
+      .then(() => this.form.reset());
   }
 
   displayUserRecord(event?: any): void {
@@ -99,11 +92,11 @@ export class UsersComponent implements OnInit {
     if (this.users.length !== 0) {
       id = this.users[this.users.length - 1].id + 1;
     }
-    return id;
+    return id
   }
 
   onSubmit(form: any): void {
-    const data: User = 
+    const data: User =
       Object.assign({ id: this.generateUserId() }, form.value)
 
     this.dataService.post(this.usersUrl, data)
